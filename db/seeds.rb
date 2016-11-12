b6128accb26c07cb28b6c9d26bbf2ef36b5b0b8b
John Bell <wahwho@yahoo.com>
2016-11-12 14:02:41 -0500

    added seed for workflows

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Workflow.destroy_all
+
+Workflow.create(
+    [
+        {
+            name: "Test Workflow 1",
+            description: "This is a sample workflow. If you see it and don't know why it's here, it should be deleted.",
+            version: "1"
+        },
+        {
+            name: "Test Workflow 2",
+            description: "This is a another sample workflow. If you see it and don't know why it's here, it should be deleted.",
+            version: "1"
+        }
+    ]
+)
+
+p "Created #{Workflow.count} Workflows"
