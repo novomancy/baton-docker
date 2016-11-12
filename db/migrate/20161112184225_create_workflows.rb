dd508b796dc2b56b09547d2673ee0274a76489c2
John Bell <wahwho@yahoo.com>
2016-11-12 13:48:47 -0500

    ran workflow db migration

--- db/migrate/20161112184225_create_workflows.rb
+++ db/migrate/20161112184225_create_workflows.rb
@@ -1,7 +1,6 @@
 class CreateWorkflows < ActiveRecord::Migration[5.0]
   def change
     create_table :workflows do |t|
-      t.primary_key :id
       t.string :name
       t.text :description
       t.integer :version
